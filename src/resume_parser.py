9fa7302e8637d9a096ead6fd61053429fc726fa4
Yathish Kumar <yathish.kumarb07@gmail.com>
2026-06-17 21:04:18 +0000

    Added resume parser

---
+++ src/resume_parser.py
@@ -0,0 +1,15 @@
+
+print("Resume Parser Started")
+
+def extract_skills():
+    skills = [
+        "Python",
+        "FastAPI",
+        "Docker",
+        "Kubernetes",
+        "PostgreSQL"
+    ]
+    return skills
+
+if __name__ == "__main__":
+    print(extract_skills())
